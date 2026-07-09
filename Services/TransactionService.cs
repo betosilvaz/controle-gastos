82e380df1fb276244d5f95e2f09b726c7f104672
Gilberto Silva <gilbertozn527@gmail.com>
2026-07-09 15:12:56 -0300

    feat: cria o TransactionService, responsável por centralizar a lógica de negócio complexa do sistema relacionada a entidade Transaction

---
+++ Services/TransactionService.cs
@@ -0,0 +1,35 @@
+namespace ControleGastos.Services;
+
+using ControleGastos.DTOs;
+using ControleGastos.Models;
+using ControleGastos.Repositories;
+
+public class TransactionService
+{
+    private readonly ITransactionRepository transactionRepository;
+    private readonly IPersonRepository personRepository;
+
+    public TransactionService(ITransactionRepository transactionRepository, IPersonRepository personRepository) {
+        this.transactionRepository = transactionRepository;
+        this.personRepository = personRepository;
+    }
+
+    public async Task Create(CreateTransactionDto dto) {
+        var person = await personRepository.FindById(dto.PersonId);
+
+        if (person == null)
+            throw new Exception("Pessoa não encontrada.");
+
+        if (person.Age < 18 && dto.Type == TransactionType.Income)
+            throw new Exception("Menores não podem cadastrar receitas.");
+
+        var transaction = new Transaction {
+            Description = dto.Description,
+            Value = dto.Value,
+            Type = dto.Type,
+            PersonId = dto.PersonId
+        };
+
+        await transactionRepository.Add(transaction);
+    }
+}
