c9267f047d382612248e3a294c85234e617fbd82
Gilberto Silva <gilbertozn527@gmail.com>
2026-07-09 14:08:07 -0300

    feat: cria models das entidades do sistema

---
+++ Models/Transaction.cs
@@ -0,0 +1,8 @@
+public class Transaction {
+    public int Id { get; set; }
+    public string Description { get; set; } = "";
+    public decimal Value { get; set; }
+    public TransactionType Type { get; set; }
+    public int PersonId { get; set; }
+    public Person Person { get; set; } = null!;
+}
