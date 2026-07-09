a5d14f79e6f4aaa905c52c65e1548d110ecd031b
Gilberto Silva <gilbertozn527@gmail.com>
2026-07-09 14:11:06 -0300

    feat: cria o DbContext para gerenciar comunicação com o banco de dados

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext {
+    public AppDbContext(DbContextOptions<AppDbContext> options): base(options) {}
+
+    public DbSet<Person> Persons => Set<Person>();
+    public DbSet<Transaction> Transactions => Set<Transaction>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        modelBuilder.Entity<Person>()
+            .HasMany(p => p.Transactions)
+            .WithOne(t => t.Person)
+            .HasForeignKey(t => t.PersonId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
