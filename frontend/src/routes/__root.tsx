8a5e9c351e82427fb97c7f1f5cfc234e02a84303
Gilberto Silva <gilbertozn527@gmail.com>
2026-07-10 00:09:16 -0300

    feat: adiciona react-hot-toaster ao projeto

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,9 +1,12 @@
 import { createRootRoute, HeadContent, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
+import { Toaster } from 'react-hot-toast'
+
 export const Route = createRootRoute({
   component: () => (
     <>
+      <Toaster/>
       <HeadContent />
       <Outlet />
       <TanStackRouterDevtools />
